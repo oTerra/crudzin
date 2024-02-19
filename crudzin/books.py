7ae2b91e2b8ef1f44346b4f19f184ccbf52ee271
oTerra <tdfvini@gmail.com>
2024-02-19 16:46:45 -0300

    GET ALL is running

---
+++ crudzin/books.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, current_app, request, jsonify
+from .model import Book
+from .serealizer import BookSchema
+
+bp_books = Blueprint('books', __name__)
+
+
+@bp_books.route('/', methods=['GET'])
+def all():
+    bs = BookSchema(many=True)
+    result = Book.query.all()
+
+    return bs.jsonify(result), 200
+
+@bp_books.route('/delete/<id>', methods=['DELETE'])
+def delete(id):
+    Book.query.filter(Book.id == id).delete
+    current_app.db.session.commit()
+
+    return jsonify('Deleted!')
+
+@bp_books.route('/update/<id>', methods=['POST'])
+def update(id):
+    ...
+
+@bp_books.route('/create', methods=['POST'])
+def create():
+    bs = BookSchema()
+    print(bs.load())
+    book, error = bs.load(request.json)
+    current_app.db.session.add(book)
+    current_app.db.session.commit()
+
+    return bs.jsonify(book), 200
